c5c3fccbd36935220f05d141a8d0a1d94a033545
Github@12081994 <ramodedra.work@gmail.com>
2019-11-26 13:13:28 +0530

    Initial Commit

---
+++ EncryptDecrypt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EncryptDecrypt
+//
+//  Created by Rao-Mac-3 on 26/11/19.
+//  Copyright © 2019 Rao-Mac-3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
